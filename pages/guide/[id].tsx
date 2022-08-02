982a2111534b119303ee097c252bc736c6606861
Zachary Belford <belfordz66@gmail.com>
2022-08-02 14:16:44 -0700

    Added monaco for multiline code blocks

--- pages/guide/[id].tsx
+++ pages/guide/[id].tsx
@@ -2,120 +2,78 @@ import getPages, { getGuideList, Page } from '../../lib/getPages';
 import Sidenav from '../../layout/Sidenav';
 import { MDXRemote } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
-import path from 'path';
 import Editor from "@monaco-editor/react";
-import {renderToStaticMarkup} from 'react-dom/server';
-import React from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 import * as monacoEditor from 'monaco-editor/esm/vs/editor/editor.api';
 
-const pagesDirectory = path.join(process.cwd(), 'guide');
-
 type PropTypes = {
-  children: React.ReactElement,
   defaultValue: string,
 };
 
+function CodeBlock(props: any) {
+  const editorOptions = {
+    scrollbar: {
+      // Render vertical arrows. Defaults to false.
+      verticalHasArrows: true,
+      // Render horizontal arrows. Defaults to false.
+      horizontalHasArrows: true,
+      // Render vertical scrollbar.
+      // Accepted values: 'auto', 'visible', 'hidden'.
+      // Defaults to 'auto'
+      vertical: "hidden",
+      // Render horizontal scrollbar.
+      // Accepted values: 'auto', 'visible', 'hidden'.
+      // Defaults to 'auto'
+      horizontal: "hidden",
+      verticalScrollbarSize: 17,
+      horizontalScrollbarSize: 17,
+      arrowSize: 30,
+      useShadows: false,
+    },
+    minimap: {
+      enabled: false,
+    },
+    showFoldingControls: "never",
+    peekWidgetDefaultFocus: "editor",
+    scrollBeyondLastLine: false,
+    lineNumbers: "off",
+    fixedOverflowWidgets: true,
+  } as monacoEditor.editor.IEditorConstructionOptions;
 
-const LINE_HEIGHT = 18;
-const DEFAULT_STATE = {
-    editor: undefined as unknown as monacoEditor.editor.ICodeEditor,
-    prevLineCount: -1
-}
+  const MAX_HEIGHT = 600;
+  const MIN_COUNT_OF_LINES = 3;
+  const LINE_HEIGHT = 19;
 
-class InlineMonacoEditor extends React.Component<PropTypes, typeof DEFAULT_STATE> {
-    constructor(props: PropTypes) {
-        super(props);
-        this.state = DEFAULT_STATE;
+  const [height, setHeight] = useState(170);
+  const valueGetter = useRef();
+
+  const handleEditorChange = useCallback(() => {
+    console.log("handle editor change");
+    const countOfLines = (valueGetter as any).current.getValue().split("\n").length;
+    if (countOfLines >= MIN_COUNT_OF_LINES) {
+      const currentHeight = countOfLines * LINE_HEIGHT;
+      if (MAX_HEIGHT > currentHeight) {
+        setHeight(currentHeight);
+      }
     }
+  }, []);
 
-    componentWillUnmount() {
-        if (window) {
-            window.removeEventListener('resize', this.setEditorHeight);
-        }
-    }
+  const handleEditorDidMount = useCallback((editor: any) => {
+    valueGetter.current = editor;
+    handleEditorChange();
+    editor.onDidChangeModelContent(handleEditorChange);
+  }, [handleEditorChange]);
 
-    render() {
-        // override a word wrapping, disable and hide the scroll bars
-        const optionsOverride = {
-            wordWrap: 'on',
-            scrollBeyondLastLine: false,
-            scrollbar: {
-                vertical: 'hidden',
-                horizontal: 'hidden',
-            },
-        } as monacoEditor.editor.IEditorConstructionOptions;
+  const code = props.children.props.children;
+  console.log(code);
 
-        return (
-            <Editor
-                {...this.props}
-              defaultLanguage="javascript"
-                onMount={this.editorDidMount()}
-                options={optionsOverride} />
-        );
-    }
-
-    private editorDidMount(): any {
-        return (editor: any, monaco: any) => {
-            // put the edit in the state for the handler.
-            this.setState({ editor });
-
-            // do the initial set of the height (wait a bit)
-            setTimeout(this.setEditorHeight, 0);
-
-            // adjust height when the window resizes
-            if (window) {
-                window.addEventListener("resize", this.setEditorHeight);
-            }
-
-            // on each edit recompute height (wait a bit)
-            editor.onDidChangeModelDecorations(() => {
-                setTimeout(this.setEditorHeight, 0)
-            });
-        };
-    }
-
-    setEditorHeight = (() => {
-        const { editor, prevLineCount } = this.state;
-        if (!editor) { return; }
-        const editorDomNode = editor.getDomNode();
-        if (!editorDomNode) { return; }
-        const container = editorDomNode.getElementsByClassName('view-lines')[0] as HTMLElement;
-        const containerHeight = container.offsetHeight;
-        const lineHeight = container.firstChild
-            ? (container.firstChild as HTMLElement).offsetHeight
-            : LINE_HEIGHT;
-
-        if (!containerHeight) {
-            // dom hasn't finished settling down. wait a bit more.
-            setTimeout(this.setEditorHeight, 0);
-        } else {
-            const currLineCount = container.childElementCount;
-            const nextHeight = (prevLineCount > currLineCount)
-                // if line count is shrinking monaco tends to leave the extra
-                // space at the end, compute the height from the line count
-                ? currLineCount * lineHeight
-                // otherwise use the height of the container div as the height
-                // of the editor node
-                : containerHeight;
-
-            // set the height and redo layout
-            editorDomNode.style.height = nextHeight + 'px';
-            editor.layout();
-
-            // double check that layout didn't change things too much
-            if (container.childElementCount !== currLineCount) {
-                this.setEditorHeight();
-            } else {
-                this.setState({ prevLineCount: currLineCount })
-            }
-        }
-    }).bind(this);// bind now so addEventListener/removeEventListener works.
-}
-
-function CodeBlock({children}: PropTypes) {
   return (
-    <InlineMonacoEditor
-      defaultValue={renderToStaticMarkup(children)}
+    <Editor
+      height={height}
+      language="javascript"
+      onMount={handleEditorDidMount}
+      defaultValue={props.children.props.children}
+      options={editorOptions}
     />
   );
 }
@@ -126,7 +84,7 @@ export default function Guide({ pages, pageData }: any) {
       <Sidenav pages={pages} />
       <div className="guide">
         <MDXRemote {...pageData.result} components={{
-          code:CodeBlock
+          pre:CodeBlock
         }} />
       </div>
     </div>
