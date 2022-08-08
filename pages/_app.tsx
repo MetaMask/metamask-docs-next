94378d9a4d72dd71b243b9759e7dc631fc49a7da
garrettbear <gwhisten@gmail.com>
2022-08-08 14:07:42 -0700

    use design token package w/ next themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 /* eslint-disable import/no-unassigned-import */
+import { ThemeProvider } from 'next-themes';
 import 'normalize.css';
 import '../styles/index.scss';
 
 import type { AppProps } from 'next/app';
 import Layout from '../layout';
 
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout {...pageProps}>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider>
+      <Layout {...pageProps}>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 };
 
