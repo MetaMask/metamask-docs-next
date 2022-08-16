f45d8494e30e5eae93a664e12f6cd1920f27266d
Zachary Belford <belfordz66@gmail.com>
2022-08-15 19:54:17 -0700

    Added sorting groups by order

--- lib/getPages.ts
+++ lib/getPages.ts
@@ -130,5 +130,15 @@ export const getTOC = async (): Promise<TOCGroup[]> => {
     });
   });
 
+  groups.sort((a, b) => {
+    if (a.order > b.order) {
+      return 1;
+    }
+    if (a.order === b.order) {
+      return 0;
+    }
+    return -1;
+  });
+
   return groups;
 };
