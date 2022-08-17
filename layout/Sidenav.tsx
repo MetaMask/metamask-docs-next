6a85dfec5976aaed3adc01b481f9a0418454d622
Zachary Belford <belfordz66@gmail.com>
2022-08-17 15:45:40 -0700

    Fixed import of TOCGroup

--- layout/Sidenav.tsx
+++ layout/Sidenav.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { TOCGroup } from '../lib/getPages';
+import { TOCGroup } from '../lib/getTOC';
 
 interface PropTypes {
   toc: TOCGroup[];
